40f03f8fe4b70afb3fb59cb8ef75d37245486dcc
Alejandro García <alejandrogarciaag41@gmail.com>
2023-03-24 14:22:37 +0100

    Commit inicial: input e-mail i contrassenya

---
+++ search_replace.py
@@ -0,0 +1,85 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+import time
+import sys
+import wordlists
+import audiolists
+
+def search_and_replace(n_entrega):
+
+    n_entrega = int(n_entrega)
+
+    if not type(n_entrega) is int:
+        raise TypeError("S'ha d'incloure el número d'una entrega com a argument")
+
+# Obrir Firefox i maestra
+    driver = webdriver.Firefox()
+    driver.get("https://app.maestrasuite.com/login")
+    assert "Maestra" in driver.title
+
+#Posar usuari i contrassenya
+#TODO: canviar per comandes bitwarden o per input manual
+    user_box = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div/div[2]/div/div[1]/input")
+    user_box.clear()
+    username = input("E-mail: \n")
+    user_box.send_keys(username)
+
+    password_box = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div/div[3]/div/div[1]/input")
+    password = input("Contrassenya: \n")
+    password_box.send_keys(password)
+    password_box.send_keys(Keys.RETURN)
+
+    time.sleep(1)
+
+    if not driver.current_url is "https://app.maestrasuite.com/voiceovers":
+        raise Exception("Usuari o contrassenya incorrectes. Torneu a iniciar el programa.")
+
+# Entrar a la pestanya de captions
+    caption_button = WebDriverWait(driver, 10).until(ec.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div[1]/div[2]/div[2]/div/div[1]/a[2]")))
+    caption_button.click()
+
+#Entrar a entrega
+    folder_button = WebDriverWait(driver, 20).until(ec.element_to_be_clickable((By.XPATH,f"//*[text()='Entrega {n_entrega}']")))
+    folder_button.click()
+
+    audiolist = audiolists.entrega_23
+
+    for audio in audiolist:
+        search_box = WebDriverWait(driver, 30).until(ec.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div[5]/div/div[1]/div/div/div[1]/div[4]/div[1]/div/div/input")))
+        search_box.clear()
+        search_box.send_keys(audio)
+        audio_button = WebDriverWait(driver, 10).until(ec.element_to_be_clickable((By.XPATH,"/html/body/div[1]/div/div[5]/div/div[3]/div[1]/div/div/div/div/div[2]")))
+        audio_button.click()
+        wordlist = wordlists.infok
+        search_button = WebDriverWait(driver, 60).until(ec.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div[4]/div/div[1]/div[2]/div/div[1]/div[5]/div")))
+        search_button.click()
+        
+        caps_button = WebDriverWait(driver, 5).until(ec.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div[4]/div/div[1]/div[2]/div/div[1]/div[5]/div/div[2]/div[1]/div[2]/button[1]")))
+        caps_button.click()
+
+        for old_word, new_word in wordlist:
+            old_word_box = WebDriverWait(driver, 10).until(ec.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div[4]/div/div[1]/div[2]/div/div[1]/div[5]/div/div[2]/div[1]/div[1]/input")))
+            old_word_box.send_keys(Keys.CONTROL + "a")
+            old_word_box.send_keys(Keys.DELETE)
+            old_word_box.send_keys(old_word)
+            time.sleep(.4)
+            word_count = WebDriverWait(driver, 3).until(ec.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div[4]/div/div[1]/div[2]/div/div[1]/div[5]/div/div[2]/div[1]/div[2]/div"))).text
+            replace_box = driver.find_element(By. XPATH, "/html/body/div[1]/div/div[4]/div/div[1]/div[2]/div/div[1]/div[5]/div/div[2]/div[2]/div[1]/input")
+            replace_all_button = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[4]/div/div[1]/div[2]/div/div[1]/div[5]/div/div[2]/div[2]/div[2]/button[2]")
+
+            if word_count != "No match":
+                replace_box.send_keys(Keys.CONTROL + "a")
+                replace_box.send_keys(Keys.DELETE)
+                replace_box.send_keys(new_word) 
+                replace_all_button.click()
+                confirm_button = WebDriverWait(driver, 3).until(ec.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div[4]/div/div[8]/div/div/div[2]/div/button[1]")))
+                confirm_button.click()
+                time.sleep(1)
+
+        back_button = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[4]/div/div[1]/div[2]/div/div[1]/div[1]")
+        back_button.click()
+
+search_and_replace(sys.argv[1])
---
+++ wordlists.py
@@ -0,0 +1,79 @@
+infok = [
+        
+        #InfoK
+        # ("info k", "InfoK"),
+        # ("infocar", "InfoK"),
+        # ("infocat", "InfoK"),
+        # ("infoca", "InfoK"),
+        # ("infocap", "InfoK"),
+        # ("info caires", "infokaires"),
+        ("T V tres", "TV3"),
+        # ("súper ", "super"),
+        # ("crack", "crac"),
+
+        #medi ambient
+        ("collserola", "Collserola"),
+        ("península ibèrica", "península Ibèrica"),
+        ("C O dos", "CO2"),
+        ("c o dos", "CO2"),
+        ("eradicar", "erradicar"),
+        ("unió europea", "Unió Europea"),
+
+        #diacrítics
+        ("dón", "don"),
+        (" sóc ", " soc "),
+        (" nét", " net "),
+        ("adéu", "adeu"),
+        ("Adéu", "Adeu"),
+        (" ós", " os"),
+
+        #majúscules
+        ("barcelona", "Barcelona"),
+        ("catalunya", "Catalunya"),
+        ("espanya", "Espanya"),
+        ("àfrica", "Àfrica"),
+        ("àsia", "Àsia"),
+        ("europa", "Europa"),
+        ("La Terra", "la Terra"),
+        ("l'ebre", "l'Ebre"),
+        ("alt Empordà", "Alt Empordà"),
+        ("I A", "i a"),
+        ("nasa", "NASA"),
+        ("A A", "a, a"),
+        ("I I", "i, i"),
+        ("O A", "o a"),
+        ("Ràdio", "ràdio"),
+        ("Riu", "riu"),
+        ("Junts", "junts"),
+        ("Cuina", "cuina"),
+        ("Castell", "castell"),
+        ("La Salut", "la salut"),
+
+        #guions
+        (" vint i ", " vint-i-"),
+        ("dos cents", "dos-cents"),
+        ("dues centes", "dues-centes"),
+        ("tres cent", "tres-cent"),
+        ("quatre cent", "quatre-cent"),
+        ("cinc cent", "cinc-cent"),
+        ("sis cent", "sis-cent"),
+        ("set cent", "set-cent"),
+        ("vuit cent", "vuit-cent"),
+        ("nou cent", "nou-cent"),
+        ("r lo", "r-lo"),
+
+        #punts volats
+        ("peli", "pel·li"),
+        # (" cole ", " col·le "),
+        ("pel lícula", "pel·lícula"),
+        ("polució", "pol·lució"),
+        ("goril l", "goril·l"),
+        ("col labor","col·labor"),
+        ("il legal", "il·legal"),
+        ("al lu", "al·lu"),
+        ("il lu", "il·lu"),
+
+
+        ]
+
+
